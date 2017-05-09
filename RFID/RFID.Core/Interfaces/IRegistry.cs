239245f76407ecc0de2f29c7fbbb0469e1c235db
jeromemanzano <jeromemanzano@gmail.com>
2017-05-09 14:17:23 +0800

    Jerome Changes

---
+++ RFID/RFID.Core/Interfaces/IRegistry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RFID.Core.Interfaces
+{
+    interface IRegistry
+    {
+        String DeviceName { get; set; }
+        String Station { get; set; }
+        String Version { get; set; }
+    }
+}
